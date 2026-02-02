f0af181d90ca988a7a1176292a375b4d63dbfca2
Srinath <srinath9787672574@gmail.com>
2026-02-02 10:15:44 +0530

    Add files via upload

---
+++ code java promblem/evenorodd.java
@@ -0,0 +1,17 @@
+package practise_java;
+import java.util.Scanner;
+public class evenorodd {
+	public static void main(String[] args) {
+		Scanner num= new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int a = num.nextInt();
+		if(a/2==0) {
+		System.out.println("number is even");
+	}
+		else {
+		System.out.println("number is odd");	
+		}
+	num.close();	
+	}
+
+}
