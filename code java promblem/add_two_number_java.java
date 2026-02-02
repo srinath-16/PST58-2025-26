d81645ef2e5b72b75f4a3d9052bc67bd2fd65bd3
Srinath <srinath9787672574@gmail.com>
2026-02-02 09:54:11 +0530

    Add files via upload

---
+++ code java promblem/add_two_number_java.java
@@ -0,0 +1,11 @@
+package practise_java;
+public class add_two_number_java {
+	public static void main(String[] args) {
+			int a= 5;
+			int b= 10;
+			int c= a+b;
+			System.out.println("Addithion of two number is:"+c);
+
+			}
+
+		}
