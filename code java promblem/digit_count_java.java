1459a476d4a40e4c73b24ed71b320480af4fe463
Srinath <srinath9787672574@gmail.com>
2026-02-02 10:05:09 +0530

    Add files via upload

---
+++ code java promblem/digit_count_java.java
@@ -0,0 +1,22 @@
+package practise_java;
+	import java.util.Scanner;
+	public class digit_count_java {
+		public static void main(String[] args) {
+			Scanner in= new Scanner(System.in);
+			System.out.println("Enter the number:");
+			int num=in.nextInt();
+			int count = 0;
+		    int temp = num; 
+		    if (temp == 0) {
+		        count = 1;
+		    } 
+		    else {
+		      while (temp > 0) {
+		       temp = temp / 10; 
+		       count++;         
+		    }
+		    }
+		    System.out.println("The number of digits is: " + count);
+		    in.close();
+		}
+		}
